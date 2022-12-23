2d89b6628331bb8047e99361ef2a28af6441a14b
Sean Peng <33929745+seanpeng11@users.noreply.github.com>
2022-12-23 16:33:04 +0800

    Add button to reset and resize grid

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function makeGrid(n) {
+    n = n || 16;
+    console.log(n);
+    n = Math.max(Math.min(n, 100), 10);
+    gridContainer.replaceChildren();
     gridContainer.style.setProperty("--grid-rows", n);
     gridContainer.style.setProperty("--grid-cols", n);
     for (let i = 0; i < n * n; i++) {
-        let cell = document.createElement("div");
+        const cell = document.createElement("div");
         cell.addEventListener("mouseover", () => cell.classList.add("grid-fill"));
         gridContainer.appendChild(cell);
     }
 }
 
+const gridContainer = document.querySelector(".grid-container");
+makeGrid(16);
 
-gridContainer = document.querySelector(".grid-container");
-
-makeGrid(16)
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => makeGrid(parseInt(prompt("Enter the size of the grid:"))));
